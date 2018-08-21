6e3eefae352f18b5584292d48e106221bf04c64d
Woo Song <ws392@drexel.edu>
2018-08-21 19:53:08 -0400

    Add homepage and update CSS

--- wp-content/themes/wooyoungsong/dist/js/main.js
+++ wp-content/themes/wooyoungsong/dist/js/main.js
@@ -16,11 +16,18 @@ jQuery(function($) {
         jQuery(".work").css("display","block");
     });
 
+    // vTciker settting that does rolling texts
     jQuery(function() {
-        jQuery('.loop__container').vTicker('init', {
+        jQuery(".loop__container").vTicker('init', {
             speed: 600, 
             pause: 2000,
             padding: 10});
     });
+
+    // Wrap all iframe in div for css style to work
+    jQuery( "iframe" ).wrap( "<div class='embed-container'></div>" );
+
+    // Necessary to remove wp style that override my custom css for image in general page
+    jQuery(".wp-caption").removeAttr('style');
 
 });
--- wp-content/themes/wooyoungsong/index.php
+++ wp-content/themes/wooyoungsong/index.php
@@ -11,21 +11,4 @@
  */
 get_header(); ?>
 
-<?php /*<main class="main">
-        <!-- Greeting -->
-        <div class="loop">
-            <div class="loop__bracket-left">[</div>
-            <div class="loop__container">
-                <ul class="loop__container__list">
-                    <li class="loop__container__list__item">Hello!</li>
-                    <li class="loop__container__list__item">안녕하세요!</li>
-                    <li class="loop__container__list__item">こんにちは!</li>
-                    <li class="loop__container__list__item">你好!</li>
-                </ul>
-            </div>
-            <div class="loop__bracket-right">]</div>
-        </div>
-
-</main>*/ ?>
-
 <?php get_footer(); ?>
